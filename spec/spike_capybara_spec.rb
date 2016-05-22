14ab3a2e4c0ea152cacecbbdebbf9b7c7f0273ab
Gabriel Russell <gdrussell81@gmail.com>
2016-05-22 15:56:46 -0400

    added external link testing for shop link

--- spec/spike_capybara_spec.rb
+++ spec/spike_capybara_spec.rb
@@ -1,24 +1,30 @@
 require 'spec_helper.rb'
 
 describe 'The website' do
+
+  pageHeader = '#comp-io0p51x4'
+  dropDownMenu = '#DrpDwnMn0'
+  etsyShopUrl = 'https://www.etsy.com/shop/PendragonChainmail'
+
   before(:all) do
     visit ''
   end
 
   it 'should show the correct page title' do
     expect(page).to have_title "pendragonchainmail"
-    expect(find('#comp-io0p51x4')).to have_text "Pendragon Chainmail"
+    expect(find(pageHeader)).to have_text "Pendragon Chainmail"
   end
 
   describe 'Menu' do
-    dropDownMenu = '#DrpDwnMn0'
-
     it 'should exist' do
       expect(page).to have_selector(dropDownMenu)
     end
 
-    it 'should have a shop link' do
-      expect(find(dropDownMenu)).to have_text "Shop"
+    it 'should have the correct links' do
+      expect(find(dropDownMenu)).to have_text "Home"
+      expect(find(dropDownMenu)).to have_text "Portfolio"
+      expect(find(dropDownMenu)).to have_text "Contact Us"
+      expect(find(dropDownMenu)).to have_link("Shop", :href => etsyShopUrl)
     end
   end
 end

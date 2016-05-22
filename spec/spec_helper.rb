4bb7fb12cc0a59299dfb1dfbe98a6ad8326cdea8
Gabriel Russell <gdrussell81@gmail.com>
2016-05-22 15:29:17 -0400

    Added tests for menu and page title. Moved setup into spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,15 @@
+require 'rspec'
+require 'capybara'
+require 'capybara/dsl'
+
 RSpec.configure do |config|
   config.include Capybara::DSL
 end
+
+# Debugging in Chrome is much easier
+Capybara.register_driver :selenium do |app|
+  Capybara::Selenium::Driver.new(app, :browser => :chrome)
+end
+
+Capybara.default_driver = :selenium
+Capybara.app_host = 'http://pendragonchainmail.com'
--- spec/spike_capybara_spec.rb
+++ spec/spike_capybara_spec.rb
@@ -1,17 +1,24 @@
-require 'rspec'
-require 'capybara'
-require 'capybara/dsl'
 require 'spec_helper.rb'
 
-Capybara.register_driver :selenium do |app|
-  Capybara::Selenium::Driver.new(app, :browser => :chrome)
-end
-
-Capybara.default_driver = :selenium
-Capybara.app_host = 'http://www.pendragonchainmail.com'
-
 describe 'The website' do
-  it 'should exist' do
+  before(:all) do
     visit ''
+  end
+
+  it 'should show the correct page title' do
+    expect(page).to have_title "pendragonchainmail"
+    expect(find('#comp-io0p51x4')).to have_text "Pendragon Chainmail"
+  end
+
+  describe 'Menu' do
+    dropDownMenu = '#DrpDwnMn0'
+
+    it 'should exist' do
+      expect(page).to have_selector(dropDownMenu)
+    end
+
+    it 'should have a shop link' do
+      expect(find(dropDownMenu)).to have_text "Shop"
+    end
   end
 end
